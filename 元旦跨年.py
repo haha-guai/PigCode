6b7e3e27816ba00acb5c8fce19369c4b8169cb3a
岁岁与 <lyfx1314520@qq.com>
2024-12-31 22:49:57 +0800

    跨年代码优化

--- 元旦跨年.py
+++ 元旦跨年.py
@@ -12,13 +12,25 @@ for friend in friend_infos:
             friend_list.append(friend['nickname'])
     except:
         pass
+print("朋友列表获取成功")
 
 time_str = '2025-01-01 00:00:00'
 time_format = '%Y-%m-%d %H:%M:%S'
 time_struct = time.strptime(time_str, time_format)
 timestamp = time.mktime(time_struct)
-while timestamp > time.time():
-    time.sleep(3)
-
+while (timestamp - time.time()) > 0:
+    if (timestamp - time.time()) >= 3600:
+        print(f"剩余：{round(timestamp - time.time()) / 60}min")
+        time.sleep(600)
+        
+    elif (timestamp - time.time()) >= 700:
+        print(f"剩余：{round(timestamp - time.time())}s")
+        time.sleep(30)
+    elif (timestamp - time.time()) >= 60:
+        printf(f"剩余：{round(timestamp - time.time())}s")
+        time.sleep(3)
+    else:
+        print(f"剩余：\033[31m{round(timestamp - time.time())}s")
+        time.sleep(1)
 for friend in friend_list:
     wx.SendMsg('元旦快乐', friend)
